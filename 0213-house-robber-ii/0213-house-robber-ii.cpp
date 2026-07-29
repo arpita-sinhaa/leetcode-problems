25b8fc012035c0420c7af7afb979493053720708
Arpita <144442131+arpita-sinhaa@users.noreply.github.com>
2026-07-30 00:22:23 +0530

    Time: 0 ms (100%), Space: 10.7 MB (59.32%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+vector<int> dp;
+
+    int solve(int i, int end, vector<int>& nums){
+        if(i>end) return 0;
+
+        if(dp[i]!=-1) return dp[i];
+
+        int skip= solve(i+1, end, nums);
+        int take= nums[i]+ solve(i+2, end, nums);
+
+        return dp[i]= max(skip, take);
+    }
+
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+
+        //imp base case
+        if(n==1) return nums[0];
+
+        //decalre for first option
+        dp.assign(n, -1);
+        int option1= solve(0, n-2, nums);
+
+        //redeclare for 2nd option as same dp can be used for boht calls
+        dp.assign(n,-1);
+        int option2= solve(1, n-1, nums);
+
+        return max(option1, option2);
+    }
+};
